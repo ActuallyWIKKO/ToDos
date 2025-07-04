6da7958c7ef2d0f11f164556a40227643e5c81cf
ActuallyWIKKO <dalineko@googlemail.com>
2025-07-04 19:37:21 +0200

    Small amount of housekeeping and refactor before the styling starts.

--- packages/ui/Footer/Footer.tsx
+++ packages/ui/Footer/Footer.tsx
@@ -1,8 +1,9 @@
 export const Footer: React.FC = () => {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+    <footer className="footer w-[250px] footer-center bg-base-300 text-base-content p-4 m-5">
       <aside>
         <p>Made with &hearts; in Hamburg City</p>
+        <p>WIKKO.dev</p>
       </aside>
     </footer>
   );
--- packages/ui/Header/Header.tsx
+++ packages/ui/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 export const Header: React.FC = () => {
   return (
-    <header className="navbar bg-base-100 shadow-sm">
+    <header>
       <div className="flex-1">
         <h1 className="text-xl">WIKKO's Task Planner</h1>
       </div>
